737157e54418f2196231324b2c3250ede1e26394
KartheeshReddy <65816735+KartheeshReddy@users.noreply.github.com>
2021-10-30 11:39:33 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "find_nearby";
+$con = mysqli_connect($servername, $username, $password, $database);
+
+
+session_start();
+
+
+?>
